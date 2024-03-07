70e548e5ad8177a10fd0f7db6601e10ddc37c385
aaronfnp <aaronfnp@gmail.com>
2024-03-07 14:15:00 -0500

    changed to single lines

--- clues.sql
+++ clues.sql
@@ -9,7 +9,7 @@ SELECT * FROM country WHERE region = 'Southern Europe' ORDER BY population ASC L
 -- this country's officially recognized language. Check our databases and find out what language is
 -- spoken in this country, so we can call in a translator to work with you.
 
-SELECT * FROM countrylanguage WHERE countrycode = 'VAT';
+SELECT * FROM countrylanguage JOIN country ON countrylanguage.countrycode = country.code WHERE countrycode = 'VAT';
 
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on
@@ -32,18 +32,15 @@ SELECT * FROM city WHERE countrycode = 'SMR' AND name != 'San Marino';
 -- parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were
 -- headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
-SELECT * FROM city WHERE name LIKE 'Ser%' ORDER BY name;
-SELECT * FROM city WHERE name = 'Serra';
+SELECT * FROM city WHERE name LIKE 'Serr%' ORDER BY name ASC;
 
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
-SELECT * FROM country WHERE code = 'BRA';
-SELECT * FROM city WHERE id = '211';
+SELECT * FROM country JOIN city ON country.capital = city.id WHERE code = 'BRA';
 -- Bras�lia
-
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to
 -- the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the
